691842ab9dcab1b453395248964a42de770f07d0
Ryo Ando <sasigume@gmail.com>
2021-03-06 02:08:29 +0900

    removed checkbox

--- src/components/anime-graph/index.tsx
+++ src/components/anime-graph/index.tsx
@@ -1,8 +1,7 @@
 import ConvertForGraph from "@/lib/graph/convert"
 import { FetchedData } from "@/models/firebase/Anime"
 import { Converted } from "@/models/graph/Converted"
-import { Box, Checkbox, Code, Divider, Flex, Slider, SliderFilledTrack, SliderThumb, SliderTrack, Stack, useSlider } from "@chakra-ui/react"
-import { useState } from "react"
+import { Box, Divider, } from "@chakra-ui/react"
 import CodeAccordion from "../common/code-accordion"
 import NivoBump from "./nivo/nivo-bump"
 import NivoLine from "./nivo/nivo-line"
@@ -15,12 +14,7 @@ const AnimeGraph = ({ dataFromFirebase }: AnimeGraphProps) => {
 
   const dataForGraph = ConvertForGraph(dataFromFirebase) as Converted
 
-  let limitBySlider = 50
-
-  const [checkedItems, setCheckedItems] = useState([true,true,true,true])
-
-  const allChecked = checkedItems.every(Boolean)
-  const isIndeterminate = checkedItems.some(Boolean) && !allChecked
+  let limitBySlider
 
   if (!dataForGraph.byScore || !dataForGraph.byPopularity) {
     return <Box>DATA IS INVALID</Box>
@@ -34,82 +28,46 @@ const AnimeGraph = ({ dataFromFirebase }: AnimeGraphProps) => {
         <Box fontSize="1rem">Fetched: {(`${dataFromFirebase.lastFetched}`)}</Box>
         <Box fontSize="1rem">Converted: {(`${dataForGraph.lastConverted}`)}</Box>
         <Divider my={8} />
-        <Slider aria-label="slider-ex-5" min={1} max={50} step={1} defaultValue={50} onChangeEnd={(val) => limitBySlider = val}>
-          <SliderTrack>
-            <SliderFilledTrack />
-          </SliderTrack>
-          <SliderThumb />
-        </Slider>
         <Divider my={8} />
         {limitBySlider}
-        <Stack>
-          <Checkbox
-            isChecked={allChecked}
-            isIndeterminate={isIndeterminate}
-            onChange={(e) => setCheckedItems([e.target.checked, e.target.checked])}
-          >
-            全選択
-      </Checkbox>
-          <Stack spacing={3} pl={6}>
-            <Checkbox
-              isChecked={checkedItems[0]}
-              onChange={(e) => setCheckedItems([e.target.checked, checkedItems[1], checkedItems[2], checkedItems[3]])}
-            >1</Checkbox>
-            <Checkbox
-              isChecked={checkedItems[1]}
-              onChange={(e) => setCheckedItems([checkedItems[0], e.target.checked, checkedItems[2], checkedItems[3]])}
-            >2 </Checkbox>
-            <Checkbox
-              isChecked={checkedItems[2]}
-              onChange={(e) => setCheckedItems([checkedItems[0], checkedItems[1], e.target.checked, checkedItems[3]])}
-            >3</Checkbox>
-            <Checkbox
-              isChecked={checkedItems[3]}
-              onChange={(e) => setCheckedItems([checkedItems[0], checkedItems[1], checkedItems[2], e.target.checked])}
-            >4</Checkbox>
-          </Stack>
-        </Stack>
         <Divider my={8} />
         <Box fontSize="2rem">スコア順</Box>
-        {checkedItems[0] && (
-          <>
-            <Box h="container.md" position="static">
-              <Box fontSize="1.6rem">順位推移</Box>
+        <>
+          <Box h="container.md" position="static">
+            <Box fontSize="1.6rem">順位推移</Box>
 
-              <NivoBump gds={dataForGraph.byScore.gdsForBump} mode="byscore" />
-            </Box>
-            <Divider my={8} />
-          </>
-        )}
-        {checkedItems[1] && (
-          <>
-            <Box h="container.md" position="static">
-              <Box fontSize="1.6rem">数値推移</Box>
-              <NivoLine gds={dataForGraph.byScore.gdsForLine} mode="byscore" />
-            </Box>
-            <Divider my={16} />
-          </>
-        )}
+            <NivoBump gds={dataForGraph.byScore.gdsForBump} mode="byscore" />
+          </Box>
+          <Divider my={8} />
+        </>
+
+        <>
+          <Box h="container.md" position="static">
+            <Box fontSize="1.6rem">数値推移</Box>
+            <NivoLine gds={dataForGraph.byScore.gdsForLine} mode="byscore" />
+          </Box>
+          <Divider my={16} />
+        </>
+
 
         <Box fontSize="2rem">人気順</Box>
-        {checkedItems[2] && (
-          <>
-            <Box h="container.md" position="static">
-              <Box fontSize="1.6rem">順位推移</Box>
 
-              <NivoBump gds={dataForGraph.byPopularity.gdsForBump} mode="bypopularity" />
-            </Box>
-            <Divider my={8} />
-          </>
-        )}
-        {checkedItems[3] && (
-          <>
-            <Box h="container.md" position="static">
-              <Box fontSize="1.6rem">数値推移</Box>
-              <NivoLine gds={dataForGraph.byPopularity.gdsForLine} mode="bypopularity" />
-            </Box>
-          </>
-        )}
+        <>
+          <Box h="container.md" position="static">
+            <Box fontSize="1.6rem">順位推移</Box>
+
+            <NivoBump gds={dataForGraph.byPopularity.gdsForBump} mode="bypopularity" />
+          </Box>
+          <Divider my={8} />
+        </>
+
+        <>
+          <Box h="container.md" position="static">
+            <Box fontSize="1.6rem">数値推移</Box>
+            <NivoLine gds={dataForGraph.byPopularity.gdsForLine} mode="bypopularity" />
+          </Box>
+        </>
+
         <Divider my={16} />
         <CodeAccordion data={dataForGraph} />
       </Box>
